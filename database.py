ab7b0949d55724b46cd18c1f84df5f1867a48cc2
cdmn137 <35605272+cdmn137@users.noreply.github.com>
2025-11-18 11:59:20 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,24 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Cdmn137:Alexvale.1@mydb.evtovyp.mongodb.net/?appName=mydb")
-client = AsyncIOMotorClient(MONGO_URL)
+
+# Conexión con PyMongo
+client = MongoClient(MONGO_URL, server_api=ServerApi('1'))
 database = client.messaging_app
 
 # Colecciones
 users_collection = database.users
 messages_collection = database.messages
 conversations_collection = database.conversations
+
+# Verificar conexión
+try:
+    client.admin.command('ping')
+    print("✅ Conectado a MongoDB!")
+except Exception as e:
+    print("❌ Error conectando a MongoDB:", e)
